395f5aa4432fabb4983a2e836eb5c9833c49312d
parientu <parientu@microfocus.com>
2018-10-24 16:21:03 +0300

    fixing the error in the form fields

--- src/app/services/investors.service.ts
+++ src/app/services/investors.service.ts
@@ -3,7 +3,9 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class InvestorService {
   invstorsBaseUrl = 'http://localhost:8080/investor';
 
